6f7cda56080db0ee8ade6eea5f50689d7c1b8508
bbogleE <ejs2415@gmail.com>
2025-06-09 09:31:37 +0900

    25.06.05
    C# 기초

---
+++ Unity_Study/Assets/Script/Test.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour
+{
+    public TextMeshProUGUI Txt_Text;
+        public string nextText;
+    public void OnClickButton()
+    { 
+
+    }
+
+
+
+
+    private string Name = "미소노 미카";
+    private string school = "트리니티 종합학원";
+    private int year = 3;
+    private int age = 17;
+    private string club = "티파티";
+    private float height = 157f;
+    private int month = 5;
+    private int day = 8;
+    private string hobby1 = "수다";
+    private string hobby2 = "액세서리";
+    private string color = "pink";
+
+
+
+    /**
+    void Start()
+    {
+        SelfIntroduction();
+    }
+    public void SelfIntroduction()
+    {
+        Debug.Log(school + "의 학생회인 " + club + "소속 " + year + "학년~☆ 딱딱한건 싫다구?");
+        Debug.Log(Name + "등장~☆ 잘 부탁해. 선생님.");
+        Debug.Log("소녀의 나이를 묻다니...\n" + age + "살...이에요...");
+        Debug.Log("나기 쨩보다는 조금 작은 " + height + "cm라구~☆ \n 그래도 세이아 쨩 보다는 훨씬 크다구?");
+        Debug.Log("어라...내 생일 말이야? ..." + month + "월" + day + "일 이에요...");
+        Debug.Log("같이 " + hobby1 + " 떨 사람 모집중!");
+        Debug.Log("아앗! 선생님! 오늘만 파는 한정 " + hobby2 + "가 있대! \n 함께 쇼핑이라든가, 어떨까나?");
+        Debug.Log("선생님은 " + color + "색 좋아해? 좋아하는구나? ...흐응... ");
+    }
+    **/
+}
