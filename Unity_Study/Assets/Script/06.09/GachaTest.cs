1332f0e79f7755d6e036dfadc2304ddf0ef03368
bbogleE <ejs2415@gmail.com>
2025-06-10 12:55:22 +0900

    Update GachaTest.cs

--- Unity_Study/Assets/Script/06.09/GachaTest.cs
+++ Unity_Study/Assets/Script/06.09/GachaTest.cs
@@ -8,10 +8,7 @@ public class GachaManagerCleanUI : MonoBehaviour // 스크립트 이름 변경
 
     private string[] star3Characters = { "아키라" };
     private string[] star2Characters = { "세이아", "시로코*테러" };
-    private string[] star1Characters = {
-        "와카모", "마리(아이돌)", "미카", "히나(드레스)",
-        "네루(교복)", "호시노(무장)", "사오리(드레스)"
-    };
+    private string[] star1Characters = {"와카모", "마리(아이돌)", "미카", "히나(드레스)", "네루(교복)", "호시노(무장)", "사오리(드레스)"};
     public void Gacha10x()
     {
         string allResultsText = "";
@@ -31,27 +28,27 @@ public class GachaManagerCleanUI : MonoBehaviour // 스크립트 이름 변경
 
     private string DrawSingleGacha()
     {
-        float randomNumber = Random.Range(0.0f, 100.0f);
+        int RandomNumber = Random.Range(0, 101);
 
         string drawnGrade = "";
         string finalDrawnCharacter = "";
 
-        if (randomNumber < 3.0f)
+        if (RandomNumber < 3)
         {
-            drawnGrade = "3성";
+            drawnGrade = "☆☆☆";
             finalDrawnCharacter = star3Characters[Random.Range(0, star3Characters.Length)];
         }
-        else if (randomNumber < 21.0f)
+        else if (RandomNumber < 21)
         {
-            drawnGrade = "2성";
+            drawnGrade = "☆☆";
             finalDrawnCharacter = star2Characters[Random.Range(0, star2Characters.Length)];
         }
         else
         {
-            drawnGrade = "1성";
+            drawnGrade = "☆";
             finalDrawnCharacter = star1Characters[Random.Range(0, star1Characters.Length)];
         }
 
-        return $"'{finalDrawnCharacter}' ({drawnGrade})을 뽑았다!";
+        return finalDrawnCharacter + drawnGrade;
     }
 }
